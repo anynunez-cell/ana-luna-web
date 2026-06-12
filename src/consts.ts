faea2dfce1793eae7acc179e93f2e9da34966e3d
Ana Nunez <ananunez@Anas-MacBook-Air.local>
2026-06-12 19:35:27 -0400

    Update Substack URL

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Ana Luna';
 export const SITE_AUTHOR_FORMAL = 'Ana Núñez González';
 export const SITE_DESCRIPTION = 'Escritora, mediadora cultural y editora. Quebec, Canadá.';
-export const SUBSTACK_URL = 'https://analuna.substack.com';
+export const SUBSTACK_URL = 'https://unlugarllamadocristalida.substack.com/';
